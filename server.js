4246d6bf421385561f1c3627ce2e68d4edfe1a6d
woxze-7 <woxzecom@gmail.com>
2025-08-20 16:42:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -30,4 +30,5 @@ app.post('/api/process-square-payment', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
